b2e03b5d562e57cd8a8a5f61f5aa1b5493ba93ba
dkm <dkm@rock-chips.com>
2020-05-14 14:31:58 +0800

    rknn-api: (version: 1.3.3) 1. Add PX30 host support.
    Change-Id: Ifc919ddb8c3c82bd8168a0e8baf1d85da2e2c444

--- rknn-api/Android/rknn_api/include/rknn_api.h
+++ rknn-api/Android/rknn_api/include/rknn_api.h
@@ -21,7 +21,7 @@ extern "C" {
 #include <stdint.h>
 
 /* RKNN API Version */
-#define API_VERSION                             "1.3.2"
+#define API_VERSION                             "1.3.3"
 
 /*
     Definition of extended flag for rknn_init.
